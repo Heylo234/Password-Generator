fa1e61064c0301c70eed78fd1eb4c342fc183a14
Heylo234 <111825278+Heylo234@users.noreply.github.com>
2023-05-06 17:57:02 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import random
+import time
+
+for i in range(100):
+    should_generate = input("Press enter to Generate")
+    if should_generate == "":
+        lowercase_letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
+                             "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+        uppercase_letters = ["A", "B", "C", "B", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q",
+                             "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+        numbers_digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+        special_characters = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[",
+                              "}", "]", "|", ":", ";", "<", ">", ".", "?", "/"]
+        final_set = [str(random.choice(lowercase_letters)), str(random.choice(lowercase_letters)),
+                     str(random.choice(lowercase_letters)), str(random.choice(lowercase_letters)),
+                     str(random.choice(uppercase_letters)), str(random.choice(uppercase_letters)),
+                     str(random.choice(uppercase_letters)), str(random.choice(uppercase_letters)),
+                     str(random.choice(numbers_digits)), str(random.choice(numbers_digits)),
+                     str(random.choice(numbers_digits)), str(random.choice(numbers_digits)),
+                     str(random.choice(special_characters)), str(random.choice(special_characters)),
+                     str(random.choice(special_characters)), str(random.choice(special_characters))]
+        print(random.choice(final_set) + random.choice(final_set) + random.choice(final_set) + random.choice(
+            final_set) + random.choice(final_set) + random.choice(final_set) + random.choice(final_set) + random.choice(
+            final_set) + random.choice(final_set) + random.choice(final_set) + random.choice(final_set) + random.choice(
+            final_set) + random.choice(final_set) + random.choice(final_set) + random.choice(final_set) + random.choice(
+            final_set))
+    else:
+        print("Closing...")
+        time.sleep(1)
+        print("Closed")
+        exit()
